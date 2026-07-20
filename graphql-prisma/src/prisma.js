2380fa7ba88b5504fa3c838c283f71c9e376febd
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 00:58:32 +0300

    feat: reformat Prisma helper functions and comments for consistency and readability

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -6,26 +6,32 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466',
 });
 
-/ prisma.query prisma.mutation prisma.subscription prisma.exists
+// prisma.query prisma.mutation prisma.subscription prisma.exists
 
 const createPostForUser = async (authorId, data) => {
-  const post = await prisma.mutation.createPost({
-    data: {
-      ...data,
-      author: {
-        connect: {
-          id: authorId
-        }
-      }
-    }
-  }, '{ id }')
-  const user = await prisma.query.user({
-    where: {
-      id: authorId
-    }
-  }, '{ id name email posts { id title published } }')
-  return user
-}
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId,
+          },
+        },
+      },
+    },
+    '{ id }',
+  );
+  const user = await prisma.query.user(
+    {
+      where: {
+        id: authorId,
+      },
+    },
+    '{ id name email posts { id title published } }',
+  );
+  return user;
+};
 
 // createPostForUser('cjjucl3yu004x0822dq5tipuz', {
 //     title: 'Great books to read',
@@ -36,19 +42,25 @@ const createPostForUser = async (authorId, data) => {
 // })
 
 const updatePostForUser = async (postId, data) => {
-  const post = await prisma.mutation.updatePost({
-    where: {
-      id: postId
+  const post = await prisma.mutation.updatePost(
+    {
+      where: {
+        id: postId,
+      },
+      data,
     },
-    data
-  }, '{ author { id } }')
-  const user = await prisma.query.user({
-    where: {
-      id: post.author.id
-    }
-  }, '{ id name email posts { id title published } }')
-  return user
-}
+    '{ author { id } }',
+  );
+  const user = await prisma.query.user(
+    {
+      where: {
+        id: post.author.id,
+      },
+    },
+    '{ id name email posts { id title published } }',
+  );
+  return user;
+};
 
 // updatePostForUser("cjjzwjkez009p0822fbvn6lui", { published: false }).then((user) => {
 //     console.log(JSON.stringify(user, undefined, 2))

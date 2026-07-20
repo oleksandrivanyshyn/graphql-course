7ffec8670992d09d96252e106d3dec34627ae936
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-20 21:11:35 +0300

    refactor: standardize formatting across project files; add trailing commas and consistent indentation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,25 +1,25 @@
-import { GraphQLServer } from 'graphql-yoga'
-import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+import { GraphQLServer } from 'graphql-yoga';
+import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        User,
-        Post,
-        Comment
-    },
-    context: {
-        db
-    }
-})
+  typeDefs: './src/schema.graphql',
+  resolvers: {
+    Query,
+    Mutation,
+    User,
+    Post,
+    Comment,
+  },
+  context: {
+    db,
+  },
+});
 
 server.start(() => {
-    console.log('The server is up!')
-})
+  console.log('The server is up!');
+});

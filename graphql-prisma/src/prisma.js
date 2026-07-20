0b9c0bde549247619844626b87c55ea41adf5bd9
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 00:52:10 +0300

    feat: refactor Prisma setup with `createPostForUser` and `updatePostForUser` helper functions, use dynamic path for typeDefs

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,54 +1,55 @@
 import { Prisma } from 'prisma-binding';
+import path from 'path';
 
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: path.join(__dirname, 'generated/prisma.graphql'),
   endpoint: 'http://localhost:4466',
 });
 
-// prisma.query prisma.mutation prisma.subscription prisma.exists
+/ prisma.query prisma.mutation prisma.subscription prisma.exists
 
-// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
+const createPostForUser = async (authorId, data) => {
+  const post = await prisma.mutation.createPost({
+    data: {
+      ...data,
+      author: {
+        connect: {
+          id: authorId
+        }
+      }
+    }
+  }, '{ id }')
+  const user = await prisma.query.user({
+    where: {
+      id: authorId
+    }
+  }, '{ id name email posts { id title published } }')
+  return user
+}
+
+// createPostForUser('cjjucl3yu004x0822dq5tipuz', {
+//     title: 'Great books to read',
+//     body: 'The War of Art',
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
 // })
 
-// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// prisma.mutation.createPost({
-//     data: {
-//         title: "GraphQL 101",
-//         body: "",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "cjjybkwx5006h0822n32vw7dj"
-//             }
-//         }
-//     }
-// }, '{ id title body published }').then((data) => {
-//     console.log(data)
-//     return prisma.query.users(null, '{ id name posts { id title } }')
-// }).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-prisma.mutation
-  .updatePost(
-    {
-      where: {
-        id: 'cjjzr3qpi00840822b1qpv8a8',
-      },
-      data: {
-        body: 'This is how to get started with Graphql...',
-        published: true,
-      },
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost({
+    where: {
+      id: postId
     },
-    '{ id }',
-  )
-  .then((data) => {
-    return prisma.query.posts(null, '{ id title body published }');
-  })
-  .then((data) => {
-    console.log(data);
-  });
+    data
+  }, '{ author { id } }')
+  const user = await prisma.query.user({
+    where: {
+      id: post.author.id
+    }
+  }, '{ id name email posts { id title published } }')
+  return user
+}
+
+// updatePostForUser("cjjzwjkez009p0822fbvn6lui", { published: false }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// })

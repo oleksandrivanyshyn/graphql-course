4fa623f2b7713c41b2afefe19f2f2cc1ba1238ed
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-20 20:35:55 +0300

    feat: add `posts` field to User type & resolver; update schema and package-lock.json

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -52,6 +52,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -106,6 +107,13 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        }
+    },
+    User:{
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
             })
         }
     }

0b01046ce53bcb9283bd1b4dc629e9e9cb5ccbd6
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-20 21:00:26 +0300

    refactor: simplify object creation in mutations using argument spread

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -172,9 +172,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age,
+        ...args,
       };
 
       users.push(user);
@@ -190,10 +188,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args,
       };
 
       posts.push(post);
@@ -212,9 +207,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post,
+        ...args,
       };
 
       comments.push(comment);

353a9fd27c656dfb044be03dcc5f21001776c7e3
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 13:48:01 +0300

    test: add test cases for querying users, fetching posts, invalid login, and invalid signup scenarios

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -2,17 +2,6 @@ import 'cross-fetch/polyfill';
 import ApolloBoost, { gql } from 'apollo-boost';
 import bcrypt from 'bcryptjs';
 import prisma from '../src/prisma';
-import server from '../src/server';
-
-let httpServer;
-
-beforeAll(async () => {
-  httpServer = await server.start({ port: 4000 });
-});
-
-afterAll(async () => {
-  await httpServer.close();
-});
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000',
@@ -21,15 +10,13 @@ const client = new ApolloBoost({
 beforeEach(async () => {
   await prisma.mutation.deleteManyPosts();
   await prisma.mutation.deleteManyUsers();
-
   const user = await prisma.mutation.createUser({
     data: {
       name: 'Jen',
       email: 'jen@example.com',
-      password: bcrypt.hashSync('Red098!@#$', 10),
+      password: bcrypt.hashSync('Red098!@#$'),
     },
   });
-
   await prisma.mutation.createPost({
     data: {
       title: 'My published post',
@@ -42,7 +29,6 @@ beforeEach(async () => {
       },
     },
   });
-
   await prisma.mutation.createPost({
     data: {
       title: 'My draft post',
@@ -83,4 +69,64 @@ test('Should create a new user', async () => {
     id: response.data.createUser.user.id,
   });
   expect(exists).toBe(true);
+});
+
+test('Should expose public author profiles', async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+  const response = await client.query({ query: getUsers });
+
+  expect(response.data.users.length).toBe(1);
+  expect(response.data.users[0].email).toBe(null);
+  expect(response.data.users[0].name).toBe('Jen');
+});
+
+test('Should expose published posts', async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `;
+  const response = await client.query({ query: getPosts });
+
+  expect(response.data.posts.length).toBe(1);
+  expect(response.data.posts[0].published).toBe(true);
+});
+
+test('Should not login with bad credentials', async () => {
+  const login = gql`
+    mutation {
+      login(data: { email: "jen@example.com", password: "red098!@#$" }) {
+        token
+      }
+    }
+  `;
+
+  await expect(client.mutate({ mutation: login })).rejects.toThrow();
+});
+
+test('Should not signup user with invalid password', async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: { name: "Andrew", email: "andrew@example.com", password: "pass" }
+      ) {
+        token
+      }
+    }
+  `;
+
+  await expect(client.mutate({ mutation: createUser })).rejects.toThrow();
 });

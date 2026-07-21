9621904bd435c2f143c7b5a4fbff7db5ca9b769d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 11:01:57 +0300

    feat: implement authentication for `myPosts`, `me`, and `post` queries, update schema definitions accordingly

--- graphql-prisma/src/utils/getUserId.js
+++ graphql-prisma/src/utils/getUserId.js
@@ -1,16 +1,19 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const getUserId = (request) => {
-    const header = request.request.headers.authorization
+const getUserId = (request, requireAuth = true) => {
+  const header = request.request.headers.authorization;
 
-    if (!header) {
-        throw new Error('Authentication required')
-    }
+  if (header) {
+    const token = header.replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'thisisasecret');
+    return decoded.userId;
+  }
 
-    const token = header.replace('Bearer ', '')
-    const decoded = jwt.verify(token, 'thisisasecret')
+  if (requireAuth) {
+    throw new Error('Authentication required');
+  }
 
-    return decoded.userId
-}
+  return null;
+};
 
-export { getUserId as default }
+export { getUserId as default };

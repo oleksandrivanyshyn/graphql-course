4a11a30cbbc5a3ed86f97b06aab73b93fd16ce4a
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 12:52:54 +0300

    feat: add `getFirstName` and `isValidPassword` utilities with corresponding unit tests

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,2 +1,31 @@
-test('This is my first test', async () => {});
-test('This is my second test', async () => {});
+import { getFirstName, isValidPassword } from '../src/utils/user.js';
+
+test('Should return first name when given full name', () => {
+  const firstName = getFirstName('Andrew Mead');
+
+  expect(firstName).toBe('Andrew');
+});
+
+test('Should return first name when given first name', () => {
+  const firstName = getFirstName('Jen');
+
+  expect(firstName).toBe('Jen');
+});
+
+test('Should reject password shorter than 8 characters', () => {
+  const isValid = isValidPassword('abc123');
+
+  expect(isValid).toBe(false);
+});
+
+test('Should reject password that contains word password', () => {
+  const isValid = isValidPassword('abcPassword098');
+
+  expect(isValid).toBe(false);
+});
+
+test('Should correctly validate a valid password', () => {
+  const isValid = isValidPassword('Test123098!');
+
+  expect(isValid).toBe(true);
+});

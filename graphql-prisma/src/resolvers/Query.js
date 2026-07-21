91bb2c5b359fcf19e812de26173723029f02b10f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 11:45:55 +0300

    feat: add sorting support with `orderBy` argument to `users`, `posts`, `myPosts`, and `comments` queries, update schema and resolvers

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -6,6 +6,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
     };
 
     if (args.query) {
@@ -26,6 +27,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId,
@@ -51,6 +53,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: true,
       },
@@ -74,6 +77,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
     };
 
     return prisma.query.comments(opArgs, info);
